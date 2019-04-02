0fa1db03929429776f92931938de414719ee6c5e
Justin <justinhwu95@gmail.com>
2019-04-02 17:19:29 -0400

    Added code input to command line and added content to API Communicator with partner Marcus Wordlaw

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,28 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+
+  film_array = []
+  response_hash["results"].find do |character|
+    if character["name"].downcase == character_name
+      film_array << character["films"]
+        end
+      end
+
+  filminfo_array = []
+  film_array.flatten.each do |film_url|
+    film_string = RestClient.get(film_url)
+    film_hash = JSON.parse(film_string)
+    filminfo_array << film_hash
+  end
+  filminfo_array
+end
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -16,11 +34,14 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |details|
+    puts details["title"]
+  end
 end
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to to our webpage"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name"
+  input = gets.chomp
+  return input.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
